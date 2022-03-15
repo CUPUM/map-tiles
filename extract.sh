969503cd86246ce3e8e17d157ee39c80314304d3
Emmanuel <emmbema@gmail.com>
2022-03-15 16:40:49 -0400

    Extract montreal tiles z16

--- extract.sh
+++ extract.sh
@@ -1,4 +1,6 @@
 # Render extract from the cropped data
 tilemaker 	--input input/montreal-crop.osm.pbf \
+			--config montreal/config.json \
+			--process montreal/process.lua \
 			--output montreal \
 			--store store
