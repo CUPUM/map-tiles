9e1a3d2a7ae0d1a7e1f52c81ba05e6a88f7ecfa1
Emmanuel <emmbema@gmail.com>
2022-03-15 16:19:30 -0400

    Restarting setup

---
+++ montreal/crop.sh
@@ -0,0 +1,6 @@
+# Do a regional pre-export of the osm.pbf
+osmium extract 	--bbox=-74.2044,45.1975,-73.1716,45.8479 \
+				--set-bounds \
+				--strategy=smart \
+				input/quebec-latest.osm.pbf \
+				--output input/quebec-cropped-latest.osm.pbf
---
+++ montreal/render.sh
@@ -0,0 +1,9 @@
+# Render from the full quebec data
+# tilemaker 	--input input/quebec-latest.osm.pbf \
+# 			--output output \
+# 			--store store
+
+# Render from the cropped data
+tilemaker 	--input input/quebec-cropped-latest.osm.pbf \
+			--output output \
+			--store store
